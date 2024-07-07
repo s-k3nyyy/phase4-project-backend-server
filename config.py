8c4f1ce911c257040e8d398641b6d4fccbadba6c
collo njvgushh <158499263+s-k3nyyy@users.noreply.github.com>
2024-07-07 21:35:26 +0300

    Update config.py

--- config.py
+++ config.py
@@ -1,4 +1,4 @@
-class Config:
-    SECRET_KEY = 'your_secret_key'  # Replace with a strong secret key
-    SQLALCHEMY_DATABASE_URI = 'sqlite:///site.db'
-    SQLALCHEMY_TRACK_MODIFICATIONS = False
+import os
+SECRET_KEY = 'your_secret_key_here'
+SQLALCHEMY_DATABASE_URI = 'sqlite:///users.db'  # SQLite for simplicity
+SQLALCHEMY_TRACK_MODIFICATIONS = False
