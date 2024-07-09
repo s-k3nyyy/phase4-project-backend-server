a7af5b4d83a5e69b513bcdfebe558ae0a4593370
kelvin <kelvinmutugi336@gmil.com>
2024-07-09 10:14:48 +0300

    update

--- config.py
+++ config.py
@@ -1,11 +1,4 @@
-<<<<<<< HEAD
-# import os
-# SECRET_KEY = 'your_secret_key_here'
-# SQLALCHEMY_DATABASE_URI = 'sqlite:///users.db'  # SQLite for simplicity
-# SQLALCHEMY_TRACK_MODIFICATIONS = False
-=======
-class Config:
-    SECRET_KEY = 'your_secret_key'  # Replace with a strong secret key
-    SQLALCHEMY_DATABASE_URI = 'sqlite:///site.db'
-    SQLALCHEMY_TRACK_MODIFICATIONS = False
->>>>>>> 3e608c08d31840a0dc354bd4886d6ce1c1f82804
+import os
+SECRET_KEY = 'your_secret_key_here'
+SQLALCHEMY_DATABASE_URI = 'sqlite:///users.db'  # SQLite for simplicity
+SQLALCHEMY_TRACK_MODIFICATIONS = False
