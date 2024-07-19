3b9c3a701a73e1407fb0691adaa540abbedc720e
s-k3nyyy <collo.c.heg@gmail.com>
2024-07-19 17:34:08 +0300

    updated

--- app.py
+++ app.py
@@ -1,7 +1,7 @@
 
 from datetime import datetime, timedelta
 import logging
-
+import os
 from flask import Flask, jsonify, request, make_response
 from flask_sqlalchemy import SQLAlchemy
 from flask_bcrypt import Bcrypt
@@ -20,7 +20,7 @@ app.config['JWT_COOKIE_CSRF_PROTECT'] = True
 app.config['JWT_ACCESS_CSRF_HEADER_NAME'] = 'X-CSRF-TOKEN'
 app.config['JWT_COOKIE_SECURE'] = True  # Ensure HTTPS
 app.config['JWT_COOKIE_SAMESITE'] = 'None'
-
+app.config['SQLALCHEMY_DATABASE_URI'] = os.getenv('DATABASE_URL', 'sqlite:///app.db')
 # Initialize extensions
 db = SQLAlchemy(app)
 bcrypt = Bcrypt(app)
